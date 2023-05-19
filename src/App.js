238c91b392b3bc693f63be06a0109d555371f0fd
Jason Mok <mokjason1@gmail.com>
2023-05-19 04:14:31 +0000

    Initial Commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there on9!
         </p>
         <a
           className="App-link"
